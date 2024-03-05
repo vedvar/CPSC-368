00665fe23e5b4430ab74b122eb345babd09695c4
Ved Varshney <vedvarshney@gmail.com>
2024-03-05 13:56:37 -0800

    added function to convert csv file to an SQL file

---
+++ 4/dynamically_generate.py
@@ -0,0 +1,18 @@
+import csv
+
+w = open("sql_table.sql", "a")
+
+def csv_to_sql_insert(csv_filename, table_name):
+    with open(csv_filename, 'r') as f:
+        reader = csv.reader(f)
+        columns = next(reader)
+        for row in reader:
+            columns_str = ', '.join(columns)
+            values_str = ', '.join(f"'{value}'" for value in row)
+            sql = f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str});"
+            w.write(sql)
+            w.write("\n")
+
+csv_to_sql_insert("point_grey_station_data.csv", "weather_data")
+
+w.close()
