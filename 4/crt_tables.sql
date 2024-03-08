226d3924a0ce2652600a767abe19b16bee18fe52
emjneal <144731459+emjneal@users.noreply.github.com>
2024-03-08 11:00:20 -0800

    added primary keys

--- 4/crt_tables.sql
+++ 4/crt_tables.sql
@@ -5,9 +5,11 @@ CREATE TABLE hydro_data (Location_ID   VARCHAR2(100),
                          Status        VARCHAR2(100),  
                          Latitude      VARCHAR2(100),  
                          Longitude     VARCHAR2(100),  
-                         Date_Time_UTC VARCHAR2(100),  
+                         Local_Year    VARCHAR2(100),
+                         Local_Month   VARCHAR2(100),
                          Value         VARCHAR2(100),  
-                         Unit          VARCHAR2(100));
+                         Unit          VARCHAR2(100),
+                         PRIMARY KEY (Local_Year, Local_Month, Location_ID, Location_Name));
 
 
 
@@ -19,12 +21,14 @@ CREATE TABLE weather_data (x                   VARCHAR2(100),
                            LOCAL_YEAR          VARCHAR2(100), 
                            LOCAL_DAY           VARCHAR2(100), 
                            MEAN_TEMPERATURE    VARCHAR2(100), 
-                           LOCAL_MONTH         VARCHAR2(100));
+                           LOCAL_MONTH         VARCHAR2(100),
+                           PRIMARY KEY (LOCAL_YEAR, LOCAL_DAY, LOCAL_MONTH, STATION_NAME));
 
 
 DROP TABLE wildfire_data;
 CREATE TABLE wildfire_data (﻿Year           VARCHAR2(100), 
                             Total_Hectares   VARCHAR2(100), 
                             Number_of_Fires  VARCHAR2(100), 
-                            Max_Fire_Size_ha VARCHAR2(100));
+                            Max_Fire_Size_ha VARCHAR2(100),
+                            PRIMARY KEY (Year));
 
