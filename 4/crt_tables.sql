499b3dce9c4332c069b85e552f294080f01e6dd6
UBC Student <vedv@pender.students.cs.ubc.ca>
2024-03-08 10:43:54 -0800

    formatting

--- 4/crt_tables.sql
+++ 4/crt_tables.sql
@@ -1,3 +1,4 @@
+/* Drop the table before creating it to make sure that any old table is also dropped*/
 DROP TABLE hydro_data;
 CREATE TABLE hydro_data (Location_ID   VARCHAR2(100),
                          Location_Name VARCHAR2(100),  
@@ -11,19 +12,19 @@ CREATE TABLE hydro_data (Location_ID   VARCHAR2(100),
 
 
 DROP TABLE weather_data;
-CREATE TABLE weather_data (x VARCHAR2(100), 
-                           y VARCHAR2(100), 
-                           STATION_NAME VARCHAR2(100), 
+CREATE TABLE weather_data (x                   VARCHAR2(100), 
+                           y                   VARCHAR2(100), 
+                           STATION_NAME        VARCHAR2(100), 
                            TOTAL_PRECIPITATION VARCHAR2(100), 
-                           LOCAL_YEAR VARCHAR2(100), 
-                           LOCAL_DAY VARCHAR2(100), 
-                           MEAN_TEMPERATURE VARCHAR2(100), 
-                           LOCAL_MONTH VARCHAR2(100));
+                           LOCAL_YEAR          VARCHAR2(100), 
+                           LOCAL_DAY           VARCHAR2(100), 
+                           MEAN_TEMPERATURE    VARCHAR2(100), 
+                           LOCAL_MONTH         VARCHAR2(100));
 
 
 DROP TABLE wildfire_data;
-CREATE TABLE wildfire_data (﻿Year VARCHAR2(100), 
-                            Total_Hectares VARCHAR2(100), 
-                            Number_of_Fires VARCHAR2(100), 
+CREATE TABLE wildfire_data (﻿Year           VARCHAR2(100), 
+                            Total_Hectares   VARCHAR2(100), 
+                            Number_of_Fires  VARCHAR2(100), 
                             Max_Fire_Size_ha VARCHAR2(100));
 
